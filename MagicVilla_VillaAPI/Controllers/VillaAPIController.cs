be51a4604d75f523163b62f4671ce6a27aeb2681
marys <maryshiny16@gmail.com>
2025-07-22 11:59:57 +0530

    Added necessary folders and files

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,18 @@
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<VillaDTO> GetVillas()
+        {
+            return VillaStore.villaList;
+        }
+    }
+}
---
+++ MagicVilla_VillaAPI/Models/Villa.cs
@@ -0,0 +1,9 @@
+﻿namespace MagicVilla_VillaAPI.Models
+{
+    public class VillaDTOVilla
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
